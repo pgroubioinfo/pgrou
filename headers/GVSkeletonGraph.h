3c3ec6136cbabbb66fa374532cd2b721128120fa
Thomas Crevoisier <crevoisier.thomas@gmail.com>
2013-10-23 00:38:46 +0200

    Merge GVGraph and GVSubGraph to build GVSkeletonGraph

--- headers/GVSkeletonGraph.h
+++ headers/GVSkeletonGraph.h
@@ -28,206 +28,34 @@ class GVSkeletonGraph {
 
 	public:
 
-        /**
-          * @brief the size in pixels of each node
-          *
-          */
+		static const qreal DotDefaultDPI;
+
 		static const qreal nodeSize;
 
-        /**
-          * @brief the minimum margin around each node (in points for graphviz)
-          *
-          */
         static const qreal sepValue;
-		
-        /**
-          * @brief GVSkeletonGraph constructor
-          * @param Agraph_t _graph a Graphviz graph
-          */
-		GVSkeletonGraph(Agraph_t *_graph);
+
+		GVSkeletonGraph(QString name, QFont font);
 
 		virtual ~GVSkeletonGraph();
-
-        // nodes
-
-        /**
-          * @brief counts the number of nodes
-          *
-          * @return int the number of nodes
-          *
-          */
-		int countNodes(void);
-
-        /**
-          * @brief adds a node, entering its name
-          *
-          * @param QString the name of the node to be added
-          *
-          */
-		void addNode(const QString& name);
-
-        /**
-          * @brief adds a list of nodes, entering their names
-          *
-          * @param QStringList the list of the names of the nodes to be added
-          */
-		void addNodes(const QStringList& names);
-
-        /**
-          * @brief removes a node, entering its name
-          *
-          * @param QString the name of the node to be removed
-          *
-          */
-		void removeNode(const QString& name);
-
-        /**
-          * @brief removes all the nodes
-          *
-          */
-		void clearNodes();
-
-        // edges
-
-        /**
-          * @brief adds an edge
-          *
-          * @brief QString the name of the edge's source
-          * @brief QString the name of the edge's target
-          *
-          */
-		void addEdge(const QString& source, const QString& target);
-
-        /**
-          * @brief removes an edge
-          *
-          * @brief QString the name of the egde's source
-          * @brief QString the name of the edge's target
-          *
-          */
-		void removeEdge(const QString& source, const QString& target);
-
-        /**
-          * @brief removes an edge, entering the pair (source, target)
-          *
-          * @brief QPair<QString, QString> the pair to be removed
-          *
-          */
-		void removeEdge(const QPair<QString, QString>& key);
 		
-		//Setter
-
-        /**
-          * @brief sets the label
-          *
-          * @param QString the label
-          *
-          */
-		void setLabel (const QString& name);
+		void GVSkeletonGraph::setGraphAttributes();
 		
-		//Layout results
-
-        /**
-          * @brief gets the list of nodes
-          * @return the list of nodes
-          *
-          */
-		QList<GVNode> nodes();
-
-        /**
-          * @brief gets the list of edges
-          * @return the list of edges
-          *
-          */
-		QList<GVEdge> edges();
-
-        /**
-          * @brief gets a graphviz edge by its source node name and target node name
-          *
-          * @param QString the name of the source node
-          * @param QString the name of the target name
-          * @return Agedge_t* a pointer to the retrieved edge
-          */
-        Agedge_t* getEdge(const QString& sourceName, const QString& targetName);
-
-        /**
-          * @brief gets a node by its name
-          *
-          * @param QString the name of the node
-          * @return Agnode_t* a pointer to the retrieved node
-          */
-        Agnode_t* getNode (const QString& name);
-
-        /**
-          * @brief gets the graphviz graph struct of this GVSkeletonGraph
-          *
-          * @return Agraph_t* a pointer to the graph
-          */
-        Agraph_t* graph();
-
-        /**
-          * @brief gets the DPI value
-          *
-          * @return qreal the DPI value
-          *
-          */
-        qreal getDPI();
-
-	protected:
-		GVSkeletonGraph();
-
-        /**
-          * @brief sets the Graph Attributes
-          *
-          */
-		void setGraphAttributes(void);
-
-        /**
-         * @brief Agraph_t the related Graphviz graph
-         */
-        Agraph_t* _graph;
-
-
-
-		QMap<QString, Agnode_t*> _nodes;
-		QMap<QPair<QString, QString>, Agedge_t*> _edges;
+		void GVSkeletonGraph::applyLayout()
+		
+		QList<GVNode> GVSkeletonGraph::nodes();
+		
+		void GVSkeletonGraph::addNode(const QString& name);
+		
+		GVSkeletonGraph::removeNode(const QString& name);
+		
+		GVSkeletonGraph::hasNode(const QString& name);
+		
+		GVSkeletonGraph::getNode(const QString& name);
+		
+		void GVSkeletonGraph::clearNodes();
+		
+		qreal GVSkeletonGraph::getDPI();
+		
+		Agraph_t* GVSkeletonGraph::graph();
 
 };
-
-
-// utils
-/// opens a graph
-static inline Agraph_t* _agopen(QString name, int kind) {
-	return agopen(const_cast<char *>(qPrintable(name)), kind);
-}
-
-/// adds an alternative value parameter to the method for getting an object's attribute
-static inline QString _agget(void *object, QString attr, QString alt=QString()) {
-	QString str=agget(object, const_cast<char *>(qPrintable(attr)));
-	if(str==QString())	return alt;
-	else				return str;
-}
-
-/// directly uses agsafeset which always works, contrary to agset
-static inline int _agset(void *object, QString attr, QString value) {
-	return agsafeset(object, const_cast<char *>(qPrintable(attr)),
-					 const_cast<char *>(qPrintable(value)),
-					 const_cast<char *>(qPrintable(value)));
-}
-
-/// sets node attribute
-static inline Agsym_t* _agnodeattr(Agraph_t* object, QString attr, QString value) {
-	return agnodeattr(object, const_cast<char *>(qPrintable(attr)),
-					 const_cast<char *>(qPrintable(value)));
-}
-
-/// sets edge attribute
-static inline Agsym_t* _agedgeattr(Agraph_t* object, QString attr, QString value) {
-	return agedgeattr(object, const_cast<char *>(qPrintable(attr)),
-					 const_cast<char *>(qPrintable(value)));
-}
-
-// layout
-static inline void _gvLayout(GVC_t* context, Agraph_t* graph, QString alg) {
-	gvLayout(context, graph, const_cast<char *>(qPrintable(alg)));
-}
--- src/gviz/GVSkeletonGraph.cpp
+++ src/gviz/GVSkeletonGraph.cpp
@@ -2,203 +2,125 @@
 #include <graphviz/gvc.h>
 #include <boost/make_shared.hpp>
 #include <QStringList>
+#include <QRectF>
 #include "Exceptions.h"
 #include "GVSkeletonGraph.h"
-#include "MainWindow.h"
 
-// UTILS
-
-// add subgraph
-static inline Agraph_t* _agsubg(Agraph_t* object, QString name) {
-	return agsubg(object, const_cast<char *>(qPrintable(name)));
-}
-
-// add node
-static inline Agnode_t* _agnode(Agraph_t* object, QString name) {
-	return agnode(object, const_cast<char *>(qPrintable(name)));
-}
-
-
-// CONSTRUCTORS
-
-GVSkeletonGraph::GVSkeletonGraph(Agraph_t* graph) : _graph(graph) {	
-	setGraphAttributes();
-}
-
-GVSkeletonGraph::~GVSkeletonGraph() {    
-    agclose(_graph);
-}
-
-
-//Set basic graph attributes for graphviz
+const qreal GVSkeletonGraph::DotDefaultDPI=72.0;
 const qreal GVSkeletonGraph::nodeSize = 50;
 const qreal GVSkeletonGraph::sepValue = 12.0;
 
-void GVSkeletonGraph::setGraphAttributes(void) {
-
-    //Selection of display mode
-    QString mode;
-
-    if(MainWindow::mwThis->getDisplayMode() == 0){
-
-        //Low
-        mode = "false";
-    }
-    else if(MainWindow::mwThis->getDisplayMode() == 1){
-
-        //High
-        mode = "true";
-    }
-
-    // set graph attributes
-    _agset(_graph, "overlap", "scale");
-    _agset(_graph, "splines", mode);
-    //_agset(_graph, "splines", "curved"); // with fdp, "curved" provokes edge overlap on nodes
-	_agset(_graph, "dpi", "96,0");
-    QString strSepValue = QString::number(sepValue).prepend("+");
-    _agset(_graph, "sep", strSepValue);
-
-    // divide the wanted width by the DPI to get the value in points
-    QString nodePtsWidth = QString("%1").arg(GVSubGraph::nodeSize/_agget(_graph, "dpi", "96,0").replace(',', ".").toDouble());
-	//GV uses , instead of . for the separator in floats
-    _agnodeattr(_graph, "width", nodePtsWidth.replace('.', ","));
-
+static inline Agnode_t* _agnode(Agraph_t* object, QString name){
+	return agnode(object, const_cast<char *>(qPrintable(name)));
 }
 
-//Set name
-void GVSkeletonGraph::setLabel (const QString& name) {
-	_agset(_graph, "label", name);
+static inline Agraph_t* _agopen(QString name, int kind){
+	return agopen(const_cast<char *>(qPrintable(name)),kind);
 }
 
-//Retrieve nodes for drawing (after layout has been done)
-QList<GVNode> GVSkeletonGraph::nodes() {	
+static inline QString _agget(void *object, QString attr, QString alt=QString()){
+	QString str = agget(object, const_cast<char *>(qPrintable(attr)));
+	if(str==QString()) return alt;
+	else return str;
+}
+
+static inline int _agset(void *object, QString attr, QString value){
+	return agsafeset(object, const_cast<char *>(qPrintable(attr)),const_cast<char *>(qPrintable(value)),const_cast<char *>(qPrintable(value)));
+}
+
+static inline Agsym_t* _agnodeattr(Agraph_t* object, QString attr, QString value){
+	return agnodeattr(object, const_cast<char *>(qPrintable(attr)),const_cast<char *>(qPrintable(value)));
+}
+
+static inline Agsym_t* _agedgeattr(Agraph_t* object, QString attr, QString value){
+	return agedgeattr(object, const_cast<char *>(qPrintable(attr)),const_cast<char *>(qPrintable(value)));
+}
+
+static inline _gvLayout(GVC_t* context, Agraph_t* graph, QString alg){
+	gvLayout(context, graph, const_cast<char *>(qPrintable(alg)));
+}
+
+GVSkeletonGraph::GVSkeletonGraph(QString name, QFont font){
+	_context = gvContext();
+	_graph = _agopen(name, AGDIGRAPHSTRICT);
+	setGraphAttributes();
+	setFont(font);
+}
+
+GVSkeletonGraph::~GVSkeletonGraph(){
+	gvFreeLayout(_context,_graph);
+	agclose(_graph);
+	gvFreeLayout(_context);
+}
+
+void GVSkeletonGraph::setGraphAttributes(){
+	
+	_agset(_graph,"dpi","96,0");
+	QString strSepValue = QString::number(sepValue).prepent("+");
+	_agset(_graph, "sep", strSepValue);
+	
+	QString nodePtsWidth = QString("%1").arg(GVSkeletonGraph::nodeSize/_agget(_graph,"dpi", "96,0").replace(',',".").toDouble());
+	_agnodeattr(_graph, "width", nodePtsWidth.replace('.',","));
+}
+
+void GVSkeletonGraph::applyLayout(){
+	gvFreeLayout(_context, _graph);
+	_gvLayout(_context, _graph, "fdp");
+}
+
+QList<GVNode> GVSkeletonGraph::nodes(){
 	QList<GVNode> list;
 	qreal dpi = this->getDPI();
-    for (QMap<QString, Agnode_t*>::const_iterator it = _nodes.begin(); it != _nodes.end(); ++it) {
-        Agnode_t *node = it.value();
-        GVNode object;
-        object.name = node->name; //Node name
-		//Position
-        qreal x =  node->u.coord.x * (dpi/GVGraph::DotDefaultDPI);
-        qreal y = -node->u.coord.y * (dpi/GVGraph::DotDefaultDPI);
-        object.centerPos = QPoint(x, y);
-		//Size
-        object.height = node->u.height * dpi;
-        object.width = node->u.width * dpi;
-        list << object;
-    }
-
-    return list;
+	for(QMap<QString, Agnode_t*>::const_iterator it = _nodes.begin(); it != _nodes.end(); ++it){
+		Agnode_t *node = it.value();
+		GVNode object;
+		object.name = node->name;
+		
+		qreal x = node ->u.coord.x *(dpi/GVSkeletonGraph::DotDefaultDPI);
+		qreal y = -node ->u.coord.y *(dpi/GVSkeletonGraph::DotDefaultDPI);
+		
+		object.centerPos = QPoint(x,y);
+		
+		object.height = node->u.weight * dpi;
+		object.width = node->u.width * dpi;
+		
+		list << object;
+	}
+	
+	return list;
 }
 
-//Retrieve edges for drawing (after layout has been done)
-QList<GVEdge> GVSkeletonGraph::edges() { 
-	QList<GVEdge> list;
-	qreal dpi = this->getDPI();
-    for(QMap<QPair<QString, QString>, Agedge_t*>::const_iterator it=_edges.begin(); it!=_edges.end();++it) {
-        Agedge_t *edge=it.value();
-        GVEdge object;		
-        //Fill the source and target node names
-        object.source=edge->tail->name;
-        object.target=edge->head->name;		
-        //Calculate the path from the spline
-        if((edge->u.spl->list!=0) && (edge->u.spl->list->size%3 == 1)) {
-            //If there is a starting point, draw a line from it to the first curve point
-            if(edge->u.spl->list->sflag) {
-                object.path.moveTo	(	edge->u.spl->list->sp.x*(dpi/GVGraph::DotDefaultDPI)
-									,	(-edge->u.spl->list->sp.y)*(dpi/GVGraph::DotDefaultDPI)
-									);
-                object.path.lineTo	(	edge->u.spl->list->list[0].x*(dpi/GVGraph::DotDefaultDPI)
-									,	(-edge->u.spl->list->list[0].y)*(dpi/GVGraph::DotDefaultDPI)
-									);
-            } else
-                object.path.moveTo	(	edge->u.spl->list->list[0].x*(dpi/GVGraph::DotDefaultDPI)
-									,	(-edge->u.spl->list->list[0].y)*(dpi/GVGraph::DotDefaultDPI)
-									);
-            //Loop over the curve points
-            for(int i=1; i<edge->u.spl->list->size; i+=3)
-                object.path.cubicTo	(	edge->u.spl->list->list[i].x   * (dpi/GVGraph::DotDefaultDPI)
-									, 	(-edge->u.spl->list->list[i].y)   * (dpi/GVGraph::DotDefaultDPI)
-									,	edge->u.spl->list->list[i+1].x * (dpi/GVGraph::DotDefaultDPI)
-									,	(-edge->u.spl->list->list[i+1].y) * (dpi/GVGraph::DotDefaultDPI)
-									,	edge->u.spl->list->list[i+2].x * (dpi/GVGraph::DotDefaultDPI)
-									,	(-edge->u.spl->list->list[i+2].y) * (dpi/GVGraph::DotDefaultDPI)
-									);
-            //If there is an ending point, draw a line to it
-            if(edge->u.spl->list->eflag)
-                object.path.lineTo	(	edge->u.spl->list->ep.x*(dpi/GVGraph::DotDefaultDPI)
-									,	(-edge->u.spl->list->ep.y)*(dpi/GVGraph::DotDefaultDPI)
-									);
-        }
-
-        list << object;
-    }
-
-    return list;
+void GVSkeletonGraph::addNode(const QString& name){
+	if(_nodes.contains(name)) removeNode(name);
+	_node.insert(name, _agnode(_graph, name));
 }
 
-//Node management
-int GVSkeletonGraph::countNodes (void) { return _nodes.count(); }
-void GVSkeletonGraph::addNode(const QString& name) {
-    if(_nodes.contains(name)) removeNode(name);	
-    _nodes.insert(name, _agnode(_graph, name));
-}
-void GVSkeletonGraph::addNodes(const QStringList& names) {
-    for(int i=0; i<names.size(); ++i)
-        addNode(names.at(i));
-}
-void GVSkeletonGraph::removeNode(const QString& name) {
-    if(_nodes.contains(name)) {
-        agdelete(_graph, _nodes[name]);
-        _nodes.remove(name);
-        QList<QPair<QString, QString> >keys=_edges.keys();
-        for(int i=0; i<keys.size(); ++i)
-            if(keys.at(i).first==name || keys.at(i).second==name)
-                removeEdge(keys.at(i));
-    }
+void GVSkeletonGraph::removeNode(const QString& name){
+	if(_nodes.contains(name)){
+		agdelete(_graph, _nodes[name]);
+		_node.remove(name);
+	}
 }
 
+bool GVSkeletonGraph::hasNode(const QString& name){
+	if(_nodes.contains(name)) return true;
+	return false;
+}
 
-Agnode_t* GVSkeletonGraph::getNode (const QString& name) {
-	if(_nodes.contains(name)) return _nodes[name];
-	for (QMap<QString, GVSubGraphPtr>::const_iterator it = _subgraphs.begin(); it != _subgraphs.end(); ++it)
-		if (it.value()->hasNode(name)) return it.value()->getNode(name);
+Agnode_t* GVSkeletonGraph::getNode(const QString& name){
+	if(_nodes.contains(name)) return _node[name];
 	return NULL;
 }
-void GVSkeletonGraph::clearNodes() {
-    QList<QString> keys=_nodes.keys();
-    for(int i=0; i<keys.size(); ++i)
-        removeNode(keys.at(i));
+
+void GVSkeletonGraph::clearNodes(){
+	QList<QString> keys = _nodes.keys();
+	for(int i=0;i<keys.size();++i){
+		removeNode(keys.at(i));
+	}
 }
 
-//Edge management
-void GVSkeletonGraph::addEdge(const QString &source, const QString &target) {
-    if (hasNode(source) && hasNode(target)) {
-        QPair<QString, QString> key(source, target);
-        if(!_edges.contains(key))
-            _edges.insert(key, agedge(_graph, getNode(source), getNode(target)));
-    }
-}
-void GVSkeletonGraph::removeEdge(const QString &source, const QString &target) {
-    removeEdge(QPair<QString, QString>(source, target));
-}
-void GVSkeletonGraph::removeEdge(const QPair<QString, QString>& key) {
-    if(_edges.contains(key)) {
-        agdelete(_graph, _edges[key]);
-        _edges.remove(key);
-    }
+qreal GVSkeletonGraph::getDPI(){
+	return _agget(_graph, "dpi","96,0").replace(',',".").toDouble();
 }
 
-//Getter for graph DPI
-qreal GVSkeletonGraph::getDPI() {
-	return _agget(_graph, "dpi", "96,0").replace(',', ".").toDouble();
-}
-
-Agedge_t* GVSkeletonGraph::getEdge(const QString &sourceName, const QString &targetName) {
-    QPair<QString, QString> key(sourceName, targetName);
-    if (!_edges.contains(key)) return NULL;
-    return _edges.value(key);
-}
-
-
-Agraph_t* GVSkeletonGraph::graph() { return this->_graph; }
+Agraph_t* GVSkeletonGraph::graph() {return this->_graph;}
