cbb58c77317fb66c2370800ca84f637aeb588fcf
AurelienHP <aurelienhp@gmail.com>
2013-11-18 15:50:02 +0100

    Fixing decimal separator bug : now GraphViz is forced to use US English numeric standards.

--- src/gviz/GVSkeletonGraph.cpp
+++ src/gviz/GVSkeletonGraph.cpp
@@ -5,6 +5,7 @@
 #include <QRectF>
 #include "Exceptions.h"
 #include "GVSkeletonGraph.h"
+#include "locale.h"
 
 const qreal GVSkeletonGraph::DotDefaultDPI=72.0;
 const qreal GVSkeletonGraph::nodeSize = 1;
@@ -13,32 +14,39 @@ const qreal GVSkeletonGraph::sepValue = 30.0;
 // Utils
 
 static inline Agnode_t* _agnode(Agraph_t* object, QString name){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	return agnode(object, const_cast<char *>(qPrintable(name)));
 }
 
 static inline Agraph_t* _agopen(QString name, int kind){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	return agopen(const_cast<char *>(qPrintable(name)),kind);
 }
 
 static inline QString _agget(void *object, QString attr, QString alt=QString()){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	QString str = agget(object, const_cast<char *>(qPrintable(attr)));
 	if(str==QString()) return alt;
 	else return str;
 }
 
 static inline Agsym_t* _agnodeattr(Agraph_t* object, QString attr, QString value){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	return agnodeattr(object, const_cast<char *>(qPrintable(attr)),const_cast<char *>(qPrintable(value)));
 }
 
 static inline Agsym_t* _agedgeattr(Agraph_t* object, QString attr, QString value){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	return agedgeattr(object, const_cast<char *>(qPrintable(attr)),const_cast<char *>(qPrintable(value)));
 }
 
 static inline void _gvLayout(GVC_t* context, Agraph_t* graph, QString alg){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	gvLayout(context, graph, const_cast<char *>(qPrintable(alg)));
 }
 
 GVSkeletonGraph::GVSkeletonGraph(QString name, QFont font) {
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	_context = gvContext();
 	_graph = _agopen(name, AGDIGRAPHSTRICT);
 	setGraphAttributes();
@@ -46,12 +54,14 @@ GVSkeletonGraph::GVSkeletonGraph(QString name, QFont font) {
 }
 
 GVSkeletonGraph::~GVSkeletonGraph(){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	gvFreeLayout(_context,_graph);
 	agclose(_graph);
 	gvFreeContext(_context);
 }
 
 void GVSkeletonGraph::setGraphAttributes(){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	
 	setGraphObjectAttributes(_graph,"dpi","96,0");
 	QString strSepValue = QString::number(sepValue).prepend("+");
@@ -59,27 +69,32 @@ void GVSkeletonGraph::setGraphAttributes(){
 }
 
 void GVSkeletonGraph::setGraphObjectAttributes(void *object, QString attr, QString value){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	agsafeset(object, const_cast<char *>(qPrintable(attr)),const_cast<char *>(qPrintable(value)),const_cast<char *>(qPrintable(value)));
 }
 
 void GVSkeletonGraph::setFont(QFont font){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	_font = font;
 	setGraphObjectAttributes(_graph,"fontname",font.family());
 }
 
 void GVSkeletonGraph::applyLayout(){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	gvFreeLayout(_context, _graph);
 	_gvLayout(_context, _graph, "dot");
 }
 
 
 void GVSkeletonGraph::exportToPng() {
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	gvRenderFilename(_context,_graph,"png","out.png");
 }
 
 // Node management
 
 QList<GVNode> GVSkeletonGraph::nodes(){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	QList<GVNode> list;
 	qreal dpi = this->getDPI();
 	for(QMap<QString, Agnode_t*>::const_iterator it = _nodes.begin(); it != _nodes.end(); ++it){
@@ -102,11 +117,13 @@ QList<GVNode> GVSkeletonGraph::nodes(){
 }
 
 void GVSkeletonGraph::addNode(const QString& name){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	if(_nodes.contains(name)) removeNode(name);
 	_nodes.insert(name, _agnode(_graph, name));
 }
 
 void GVSkeletonGraph::removeNode(const QString& name){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	if(_nodes.contains(name)){
 		agdelete(_graph, _nodes[name]);
 		_nodes.remove(name);
@@ -114,16 +131,19 @@ void GVSkeletonGraph::removeNode(const QString& name){
 }
 
 bool GVSkeletonGraph::hasNode(const QString& name){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	if(_nodes.contains(name)) return true;
 	return false;
 }
 
 Agnode_t* GVSkeletonGraph::getNode(const QString& name){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	if(_nodes.contains(name)) return _nodes[name];
 	return NULL;
 }
 
 void GVSkeletonGraph::setNodeSize(void* object, qreal width, qreal height){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	QString nodePtsWidth = QString("%1").arg(width/_agget(_graph,"dpi", "96,0").replace(',',".").toDouble());
 	setGraphObjectAttributes(object,"width",nodePtsWidth.replace('.',","));
 	QString nodePtsHeight = QString("%1").arg(height/_agget(_graph,"dpi", "96,0").replace(',',".").toDouble());
@@ -131,6 +151,7 @@ void GVSkeletonGraph::setNodeSize(void* object, qreal width, qreal height){
 }
 
 void GVSkeletonGraph::clearNodes(){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	QList<QString> keys = _nodes.keys();
 	for(int i=0;i<keys.size();++i){
 		removeNode(keys.at(i));
@@ -140,6 +161,7 @@ void GVSkeletonGraph::clearNodes(){
 // Edge management
 
 void GVSkeletonGraph::addEdge(const QString &source, const QString &target) {
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
     if (hasNode(source) && hasNode(target)) {
         QPair<QString, QString> key(source, target);
         if(!_edges.contains(key))
@@ -148,10 +170,12 @@ void GVSkeletonGraph::addEdge(const QString &source, const QString &target) {
 }
 
 void GVSkeletonGraph::removeEdge(const QString &source, const QString &target) {
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
     removeEdge(QPair<QString, QString>(source, target));
 }
 
 void GVSkeletonGraph::removeEdge(const QPair<QString, QString>& key) {
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
     if(_edges.contains(key)) {
         agdelete(_graph, _edges[key]);
         _edges.remove(key);
@@ -159,14 +183,18 @@ void GVSkeletonGraph::removeEdge(const QPair<QString, QString>& key) {
 }
 
 bool GVSkeletonGraph::connectionExists(const QString &sourceName, const QString &targetName){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
     QPair<QString,QString> firstPossibility(sourceName,targetName);
     QPair<QString,QString> secondPossibility(targetName,sourceName);
     return _edges.contains(firstPossibility)||_edges.contains(secondPossibility);
 }
 
 qreal GVSkeletonGraph::getDPI(){
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
 	return _agget(_graph, "dpi","96,0").replace(',',".").toDouble();
 }
 
-Agraph_t* GVSkeletonGraph::graph() {return this->_graph;}
+Agraph_t* GVSkeletonGraph::graph() {
+    setlocale(LC_NUMERIC,"en_US.UTF-8"); // Débug séparateur de décimales en version française
+        return this->_graph;}
 
